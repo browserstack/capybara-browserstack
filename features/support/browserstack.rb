7ffff66abd86531cca9d58b4adbbae8664443dcc
Karan Shah <64479353+karanshah-browserstack@users.noreply.github.com>
2022-10-10 18:19:20 +0530

    Fix capabilities for test

--- features/support/browserstack.rb
+++ features/support/browserstack.rb
@@ -31,7 +31,7 @@ Capybara.register_driver :browserstack do |app|
   Capybara::Selenium::Driver.new(app,
                                  browser: :remote,
                                  url: "https://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",
-                                 desired_capabilities: @caps)
+                                 options: @caps)
 end
 
 Capybara.default_driver = :browserstack
